2c67ca549c5031dcd98b73aa55d3d16f589c95f3
DanielHabenicht <daniel-habenicht@outlook.de>
2026-02-17 09:54:49 +0100

    dev environment

---
+++ ui/src/environments.development.ts
@@ -0,0 +1,5 @@
+/* Static development environment values. */
+
+export const GIT_TAG: string | null = 'v0.0.0-dev';
+export const GIT_COMMIT_HASH: string | null = 'devdevdevdevdevdevdevdevdevdevdev';
+export const DEFAULT_BACKEND_ROOT: string | null = '';
